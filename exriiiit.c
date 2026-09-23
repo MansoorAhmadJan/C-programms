ca2553a764d94ac2806bc4babc659fe4688da285
mansoorsecops <mansoor.secops360@gmail.com>
2026-09-23 22:16:00 +0500

    new changes and adding files

---
+++ exriiiit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int x=10,y=20;
+    printf("%d\n",x>y);
+    printf("%d\n",x=10);
+    printf("%d\n",y<=20);
+
+    return 0;
+}
---
+++ logicalllii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("%d\n", 1 && 0);
+printf("%d\n", 1 || 0);
+printf("%d\n", !1);
+printf("%d\n", !0);
+printf("%d\n", 5 > 3 && 2 < 4);
+printf("%d\n", 5 > 10 || 2 < 4);
+}
